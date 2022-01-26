15b2ce5616b2f2ca705eb2decda7adab49b5d08f
Suman <suman.kalia235@gmail.com>
2022-01-26 23:53:13 +0530

    download_video_option_added

--- src/components/UploadsList.js
+++ src/components/UploadsList.js
@@ -1,5 +1,14 @@
+import axios from "axios";
 import React from "react";
 import { BACKEND_URI } from "../config/constants";
+// var FileSaver = require('file-saver');
+import FileSaver from "file-saver";
+
+const getNameOfFile = (path) => {
+  const splittedPath = path.split("/");
+
+  return splittedPath[splittedPath.length - 1];
+};
 
 const UploadsList = ({ medias }) => {
   return (
@@ -21,15 +30,29 @@ const UploadsList = ({ medias }) => {
                     <td>
                       {media.videos.map((video) => {
                         return (
-                          <video
-                            preload="auto"
-                            width="320"
-                            height="240"
-                            controls
-                          >
-                            <source src={`${BACKEND_URI}${video}`} />
-                            ;Your browser does not support the video tag.
-                          </video>
+                          <>
+                            <video
+                              preload="auto"
+                              width="320"
+                              height="240"
+                              controls
+                            >
+                              <source src={`${BACKEND_URI}${video}`} />
+                              ;Your browser does not support the video tag.
+                            </video>
+                            <br />
+                            <button
+                              className="btn btn-primary"
+                              onClick={() =>
+                                FileSaver.saveAs(
+                                  `${BACKEND_URI}${video}`,
+                                  getNameOfFile(video)
+                                )
+                              }
+                            >
+                              Download
+                            </button>
+                          </>
                         );
                       })}
                     </td>
